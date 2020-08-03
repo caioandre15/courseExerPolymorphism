d8229bdfc9552c76c1dcc0e23539fbdbe8c48d3f
Caio André <caio_andre15@hotmail.com>
2020-08-03 18:41:47 -0300

    Created the Employee class in the entities package

---
+++ src/entities/Employee.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Employee {
+
+	// Attributes
+	private String name;
+	private Integer hours;
+	private Double valuePerHour;
+
+	// Builders
+	public Employee() {
+	}
+
+	public Employee(String name, Integer hours, Double valuePerHour) {
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHour = valuePerHour;
+	}
+
+	// Accessor Methods
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	// Methods
+	public Double payment() {
+		return valuePerHour * hours;
+	}
+
+}
